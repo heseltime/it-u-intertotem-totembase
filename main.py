4bafd6af8ea3b5a047150355527106753dc9c771
heseltime <jack.heseltine@gmail.com>
2024-09-02 11:37:15 +0200

    Set up basic working example

--- main.py
+++
@@ -1,4 +0,0 @@
-# This will be the InterCollider server/client consumer station ("intertotem-totembase")
-#    We will see if we need python on this end.
-
-# set up github for development and version control here too (github ssh)
