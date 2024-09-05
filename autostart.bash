c3efaa08d2d4b3587499c2c2de8c6f26272f9202
heseltime <jack.heseltine@gmail.com>
2024-09-05 12:44:38 +0200

    Tweak Sound

---
+++ autostart.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DISPLAY=:0.0
+sleep 10
+/usr/bin/sclang /home/totem/Desktop/intertotem/it-u-intertotem-totembase/startup.scd
---
+++ development/run_supercollider.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Launch SuperCollider and run the main.scd file
+
+# Define the path to your SuperCollider script
+SCSCRIPT=~/Desktop/intertotem/it-u-intertotem-totembase/main.scd
+
+# Run SuperCollider in command-line mode and execute the script
+sclang "$SCSCRIPT"
